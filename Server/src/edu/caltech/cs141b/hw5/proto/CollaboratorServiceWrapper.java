792ed706180effcf9e14bd727d080a1ad845be77
Annie Chin <achin@caltech.edu>
2012-02-29 23:06:08 -0800

    bugs fixed?

--- Server/src/edu/caltech/cs141b/hw5/proto/CollaboratorServiceWrapper.java
+++ Server/src/edu/caltech/cs141b/hw5/proto/CollaboratorServiceWrapper.java
@@ -24,163 +24,163 @@ import edu.caltech.cs141b.hw5.gwt.collab.shared.UnlockedDocument;
  */
 public class CollaboratorServiceWrapper {
 
-	// Replace hostname with the deployed App Engine address
-	String url = "http://5manithreads.appspot.com/collab/protoc";
-	
-	ProtoBufConnector conn;
-	
-	public CollaboratorServiceWrapper() {
-		conn = new ProtoBufConnector(url);
-	}
-	
-	/**
-	 * Test getting a list of documents
-	 * @return
-	 */
-	public List<DocumentMetadata> getDocumentList() {
-		
-		RequestMessage request = RequestMessage.newBuilder()
-				.setRequestType(RequestType.GET_DOCUMENT_LIST)
-				.build();
-		
-		ResponseMessage response = conn.makePostConnect(request);
-		
-		List<DocumentMetadata> docs = new ArrayList<DocumentMetadata>();
-		if (response.getStatusType() == StatusType.SUCCESS) {
-			for (DocumentMetaInfo info : response.getDocMetaList()) {
-				docs.add(DataConverter.buildDocumentMetadata(info));
-			}
-		}
-		return docs;
-	}
-	
-	/**
-	 * Lock a document by key value.
-	 * 
-	 * @param key
-	 * @return locked document
-	 * @throws LockUnavailable 
-	 * @throws InvalidRequest 
-	 */
-	public LockedDocument lockDocument(String key) throws LockUnavailable, InvalidRequest {
-		RequestMessage request = RequestMessage.newBuilder()
-				.setRequestType(RequestType.LOCK_DOCUMENT)
-				.setDocumentKey(key)
-				.build();
-		
-		ResponseMessage response = conn.makePostConnect(request);
-		
-		switch (response.getStatusType()) {
-			case SUCCESS:
-				return DataConverter.buildLockedDocument(response.getLockedDoc());
-			case LOCK_UNAVAILABLE:
-				throw new LockUnavailable(response.getMessage());	
-			default:
-				throw new InvalidRequest(response.getMessage());
-		}
-	}
-	
-	/**
-	 * Get a document by key
-	 * @param key
-	 * @return read-only document
-	 * @throws InvalidRequest
-	 */
-	public UnlockedDocument getDocument(String key) throws InvalidRequest {
-		RequestMessage request = RequestMessage.newBuilder()
-				.setRequestType(RequestType.GET_DOCUMENT)
-				.setDocumentKey(key)
-				.build();
-		
-		ResponseMessage response = conn.makePostConnect(request);
-		
-		switch (response.getStatusType()) {
-			case SUCCESS:
-				return DataConverter.buildUnlockedDocument(response.getUnlockedDoc());
-			default:
-				throw new InvalidRequest(response.getMessage());
-		}
-	}
-	
-	/**
-	 * Save a locked document
-	 * @param doc
-	 * @return unlocked document
-	 * @throws LockExpired
-	 * @throws InvalidRequest
-	 */
-	public UnlockedDocument saveDocument(LockedDocument doc) 
-			throws LockExpired, InvalidRequest {
-		RequestMessage request = RequestMessage.newBuilder()
-				.setRequestType(RequestType.SAVE_DOCUMENT)
-				.setLockedDoc(DataConverter.buildLockedDocumentInfo(doc))
-				.build();
-		
-		ResponseMessage response = conn.makePostConnect(request);
-		
-		switch (response.getStatusType()) {
-			case SUCCESS:
-				return DataConverter.buildUnlockedDocument(response.getUnlockedDoc());
-			case LOCK_EXPIRED:
-				throw new LockExpired(response.getMessage());	
-			default:
-				throw new InvalidRequest(response.getMessage());
-		}
-	}
-	
-	/**
-	 * Release lock of a locked document
-	 * @param doc
-	 * @throws LockExpired
-	 * @throws InvalidRequest
-	 */
-	public void releaseLock(LockedDocument doc) throws LockExpired, InvalidRequest {
-		RequestMessage request = RequestMessage.newBuilder()
-				.setRequestType(RequestType.RELEASE_LOCK)
-				.setLockedDoc(DataConverter.buildLockedDocumentInfo(doc))
-				.build();
-		
-		ResponseMessage response = conn.makePostConnect(request);
-		
-		switch (response.getStatusType()) {
-			case SUCCESS:
-				return;
-			case LOCK_EXPIRED:
-				throw new LockExpired(response.getMessage());	
-			default:
-				throw new InvalidRequest(response.getMessage());
-		}
-	}
-	
-	
-	public static void main(String[] args) {
-		CollaboratorServiceWrapper tester = new CollaboratorServiceWrapper();
-		
-		// Test get document list
-		List<DocumentMetadata> docs = tester.getDocumentList();
-		for (DocumentMetadata doc : docs) {
-			System.out.println(doc.getKey() + ": " + doc.getTitle());
-		}
-		
-		// Test lock the first document in the list for dt seconds then release it
-		if (!docs.isEmpty()) {
-			String key = docs.get(0).getKey();
-			try {
-			LockedDocument ld = tester.lockDocument(key);
-			System.out.println(ld.getTitle() + ":" + ld.getLockedUntil() + ":" 
-					+ ld.getLockedBy() + "\n"
-					+ ld.getContents());
-					
-			// Wait for 10 seconds
-			Thread.sleep(10000);
-			
-			// release the lock
-			tester.releaseLock(ld);
-			
-			} catch (Exception e) {
-				System.out.println(e.getMessage());
-			}
+    // Replace hostname with the deployed App Engine address
+    String url = "http://localhost:8888/collab/protoc";
+    
+    ProtoBufConnector conn;
+    
+    public CollaboratorServiceWrapper() {
+        conn = new ProtoBufConnector(url);
+    }
+    
+    /**
+     * Test getting a list of documents
+     * @return
+     */
+    public List<DocumentMetadata> getDocumentList() {
+        
+        RequestMessage request = RequestMessage.newBuilder()
+                .setRequestType(RequestType.GET_DOCUMENT_LIST)
+                .build();
+        
+        ResponseMessage response = conn.makePostConnect(request);
+        
+        List<DocumentMetadata> docs = new ArrayList<DocumentMetadata>();
+        if (response.getStatusType() == StatusType.SUCCESS) {
+            for (DocumentMetaInfo info : response.getDocMetaList()) {
+                docs.add(DataConverter.buildDocumentMetadata(info));
+            }
+        }
+        return docs;
+    }
+    
+    /**
+     * Lock a document by key value.
+     * 
+     * @param key
+     * @return locked document
+     * @throws LockUnavailable 
+     * @throws InvalidRequest 
+     */
+    public LockedDocument lockDocument(String key) throws LockUnavailable, InvalidRequest {
+        RequestMessage request = RequestMessage.newBuilder()
+                .setRequestType(RequestType.LOCK_DOCUMENT)
+                .setDocumentKey(key)
+                .build();
+        
+        ResponseMessage response = conn.makePostConnect(request);
+        
+        switch (response.getStatusType()) {
+            case SUCCESS:
+                return DataConverter.buildLockedDocument(response.getLockedDoc());
+            case LOCK_UNAVAILABLE:
+                throw new LockUnavailable(response.getMessage());   
+            default:
+                throw new InvalidRequest(response.getMessage());
+        }
+    }
+    
+    /**
+     * Get a document by key
+     * @param key
+     * @return read-only document
+     * @throws InvalidRequest
+     */
+    public UnlockedDocument getDocument(String key) throws InvalidRequest {
+        RequestMessage request = RequestMessage.newBuilder()
+                .setRequestType(RequestType.GET_DOCUMENT)
+                .setDocumentKey(key)
+                .build();
+        
+        ResponseMessage response = conn.makePostConnect(request);
+        
+        switch (response.getStatusType()) {
+            case SUCCESS:
+                return DataConverter.buildUnlockedDocument(response.getUnlockedDoc());
+            default:
+                throw new InvalidRequest(response.getMessage());
+        }
+    }
+    
+    /**
+     * Save a locked document
+     * @param doc
+     * @return unlocked document
+     * @throws LockExpired
+     * @throws InvalidRequest
+     */
+    public UnlockedDocument saveDocument(LockedDocument doc) 
+            throws LockExpired, InvalidRequest {
+        RequestMessage request = RequestMessage.newBuilder()
+                .setRequestType(RequestType.SAVE_DOCUMENT)
+                .setLockedDoc(DataConverter.buildLockedDocumentInfo(doc))
+                .build();
+        
+        ResponseMessage response = conn.makePostConnect(request);
+        
+        switch (response.getStatusType()) {
+            case SUCCESS:
+                return DataConverter.buildUnlockedDocument(response.getUnlockedDoc());
+            case LOCK_EXPIRED:
+                throw new LockExpired(response.getMessage());   
+            default:
+                throw new InvalidRequest(response.getMessage());
+        }
+    }
+    
+    /**
+     * Release lock of a locked document
+     * @param doc
+     * @throws LockExpired
+     * @throws InvalidRequest
+     */
+    public void releaseLock(LockedDocument doc) throws LockExpired, InvalidRequest {
+        RequestMessage request = RequestMessage.newBuilder()
+                .setRequestType(RequestType.RELEASE_LOCK)
+                .setLockedDoc(DataConverter.buildLockedDocumentInfo(doc))
+                .build();
+        
+        ResponseMessage response = conn.makePostConnect(request);
+        
+        switch (response.getStatusType()) {
+            case SUCCESS:
+                return;
+            case LOCK_EXPIRED:
+                throw new LockExpired(response.getMessage());   
+            default:
+                throw new InvalidRequest(response.getMessage());
+        }
+    }
+    
+    
+    public static void main(String[] args) {
+        CollaboratorServiceWrapper tester = new CollaboratorServiceWrapper();
+        
+        // Test get document list
+        List<DocumentMetadata> docs = tester.getDocumentList();
+        for (DocumentMetadata doc : docs) {
+            System.out.println(doc.getKey() + ": " + doc.getTitle());
+        }
+        
+        // Test lock the first document in the list for dt seconds then release it
+        if (!docs.isEmpty()) {
+            String key = docs.get(0).getKey();
+            try {
+            LockedDocument ld = tester.lockDocument(key);
+            System.out.println(ld.getTitle() + ":" + ld.getLockedUntil() + ":" 
+                    + ld.getLockedBy() + "\n"
+                    + ld.getContents());
+                    
+            // Wait for 10 seconds
+            Thread.sleep(10000);
+            
+            // release the lock
+            tester.releaseLock(ld);
+            
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
 
-		}
-	}
+        }
+    }
 }
